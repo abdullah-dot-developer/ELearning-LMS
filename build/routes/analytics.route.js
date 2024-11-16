9e0e32881ac921c3124d06647804ad78f0e71f85
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-11-16 15:30:55 +0500

    error resolved

--- build/routes/analytics.route.js
+++ build/routes/analytics.route.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const analytics_controller_1 = require("../controllers/analytics.controller");
-const auth_1 = require("../middleware/auth");
-const user_controller_1 = require("../controllers/user.controller");
+const analytics_controller_1 = require("../../controllers/analytics.controller");
+const auth_1 = require("../../middleware/auth");
+const user_controller_1 = require("../../controllers/user.controller");
 const router = express_1.default.Router();
 router.get("/get-users-analytics", user_controller_1.updateAccessToken, auth_1.isAuthenticated, (0, auth_1.authorizeRoles)("admin"), analytics_controller_1.userAnalytics);
 router.get("/get-courses-analytics", user_controller_1.updateAccessToken, auth_1.isAuthenticated, (0, auth_1.authorizeRoles)("admin"), analytics_controller_1.coursesAnalytics);
--- build/routes/course.route.js
+++ build/routes/course.route.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const auth_1 = require("../middleware/auth");
-const course_controller_1 = require("../controllers/course.controller");
-const user_controller_1 = require("../controllers/user.controller");
+const auth_1 = require("../../middleware/auth");
+const course_controller_1 = require("../../controllers/course.controller");
+const user_controller_1 = require("../../controllers/user.controller");
 // import { createCourse } from "../services/course.service";
 const router = express_1.default.Router();
 // Route for uploading a course
