28cb6f6c386d9d57df0624e8a3b4151fbe509648
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-11-16 16:20:35 +0500

    / removed

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://e-learning-lms-frontend.vercel.app/",
+    origin: "https://e-learning-lms-frontend.vercel.app",
     credentials: true,
   })
 );
