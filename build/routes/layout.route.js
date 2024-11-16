56a7f24a29a55133edaaf333351519b32eb5f316
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-11-16 15:34:51 +0500

    configured tsconfig

--- build/routes/layout.route.js
+++ build/routes/layout.route.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const auth_1 = require("../../middleware/auth");
-const layout_controller_1 = require("../../controllers/layout.controller");
-const user_controller_1 = require("../../controllers/user.controller");
+const auth_1 = require("../middleware/auth");
+const layout_controller_1 = require("../controllers/layout.controller");
+const user_controller_1 = require("../controllers/user.controller");
 const router = express_1.default.Router();
 router.post("/create-layout", user_controller_1.updateAccessToken, auth_1.isAuthenticated, (0, auth_1.authorizeRoles)("admin"), layout_controller_1.createLayout);
 router.put("/edit-layout", user_controller_1.updateAccessToken, auth_1.isAuthenticated, (0, auth_1.authorizeRoles)("admin"), layout_controller_1.editLayout);
--- build/routes/order.route.js
+++ build/routes/order.route.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const order_controller_1 = require("../../controllers/order.controller");
-const auth_1 = require("../../middleware/auth");
-const user_controller_1 = require("../../controllers/user.controller");
+const order_controller_1 = require("../controllers/order.controller");
+const auth_1 = require("../middleware/auth");
+const user_controller_1 = require("../controllers/user.controller");
 const router = express_1.default.Router();
 router.post("/create-order", auth_1.isAuthenticated, order_controller_1.createOrder);
 router.get("/get-orders", user_controller_1.updateAccessToken, auth_1.isAuthenticated, (0, auth_1.authorizeRoles)("admin"), order_controller_1.getAllOrders);
