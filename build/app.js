f920313a0e28e445834f3d152e5b4c530d5cc732
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2025-04-30 13:27:17 +0500

    added issues resolution

--- build/app.js
+++ build/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -14,9 +12,7 @@ const error_1 = __importDefault(require("./middleware/error"));
 const user_route_1 = __importDefault(require("./routes/user.route"));
 const course_route_1 = __importDefault(require("./routes/course.route"));
 const order_route_1 = __importDefault(require("./routes/order.route"));
-const notification_route_1 = __importDefault(
-  require("./routes/notification.route")
-);
+const notification_route_1 = __importDefault(require("./routes/notification.route"));
 const analytics_route_1 = __importDefault(require("./routes/analytics.route"));
 const layout_route_1 = __importDefault(require("./routes/layout.route"));
 const express_rate_limit_1 = require("express-rate-limit");
@@ -24,18 +20,16 @@ dotenv_1.default.config();
 exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
-exports.app.use(
-  (0, cors_1.default)({
+exports.app.use((0, cors_1.default)({
     origin: "https://e-learning-lms-frontend.vercel.app", //https://e-learning-lms-frontend.vercel.app
     credentials: true,
-  })
-);
+}));
 // api requests limit
 const limiter = (0, express_rate_limit_1.rateLimit)({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: "draft-7",
-  legacyHeaders: false,
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
 });
 //routes
 exports.app.use("/api/v1", user_route_1.default);
@@ -45,13 +39,13 @@ exports.app.use("/api/v1", notification_route_1.default);
 exports.app.use("/api/v1", analytics_route_1.default);
 exports.app.use("/api/v1", layout_route_1.default);
 exports.app.use("/test", (req, res) => {
-  res.send("Api is working fine");
+    res.send("Api is working fine");
 });
 //unknown route
 exports.app.all("*", (req, res, next) => {
-  const err = new Error(`Route ${req.originalUrl} not found!`);
-  err.statusCode = 404;
-  next(err);
+    const err = new Error(`Route ${req.originalUrl} not found!`);
+    err.statusCode = 404;
+    next(err);
 });
 exports.app.use(limiter);
 exports.app.use(error_1.default);
